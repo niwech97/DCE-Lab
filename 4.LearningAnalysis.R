4effe8595d321dedb979929df99175b6d2a93f4b
niwech97 <niwech97@gmail.com>
2020-04-04 08:09:44 -0400

    starting to understand!
    I use STT: to denote comments from my second time through after reading and understanding stats and the readouts more

--- 4.LearningAnalysis.R
+++ 4.LearningAnalysis.R
@@ -68,17 +68,65 @@ ds[1:10,]
 # the data
 
 #setting the formula
-fm<-RES ~ASC +x2 + x3+y2 + y3+Z+ strata(STR)
+fm <- RES ~ASC +x2 + x3+y2 + y3+Z+ strata(STR)
 
 #setting the cl
-cl<-clogit(fm,data = ds)
+cl <- clogit(fm,data = ds)
 
 
-
+#next we put it all together and call cl
 fm <- RES ~ASC +X2 + X3+Y2 + Y3+Z+ strata(STR)
 cl <- clogit(fm, data = ds)
-fm
-  clogit(fm, data = ds)
-  
-str(fm)
-str(ds)
+cl
+
+# It worked, after a bit of help from Thor(thanks Thor), but I still feel like I 
+# don't totally understand its readout or forumula. Lets try to break it down:
+# As best I can figure through reading the textbook, the RES is the choice that 
+# the respondent made, and the formula ~ notation means that it wants to compare 
+# this choice (TRUE or FALSE) to the levels involved (ASC,X2,X3,etc.) and it wants
+# to keep track of the unique ID for that choice (STR). I think this makes sense,
+
+# STT: the left side of the ~ denotes the independent variable, and the right 
+# denotes the dependent variables
+cl
+# Lets break down the readout from cl:
+# there are 6 columns, the first column is the attribute or level
+# the second column is the estimated coefficient value as given by the formula
+# the thir column is the exponential function of the estimated coeffecient
+# fourth is the standard error of the estimated coefficient
+# the last two are the z scores and p scores of the estimation under the null 
+# hypothesis
+
+# apparently there is more that is done in this function but it only spits out 
+# this summary. Additionally, we can pull out a variance matrix that can tell us 
+# more about the variance between the levels/attributes.
+# keep in mind this whole time Z is shown as a continuous variable
+# we pull variance information with this call:
+cl$var
+
+
+# after creating these estimated coeffecients, we can performa a goodness of fit 
+# test using the gofm()function.
+gofm(cl)
+
+# This code spits out a ton of statistics information that I do not yet understand
+# It seems there are definitions in earlier chapters that I'll go back to read
+
+
+# next in the analysis we can find a willingness to pay metric for each variable
+# we do that using the mwtp() function and defining our monetary vs nonmonetary 
+# variables
+
+mwtp.cl<-mwtp(output = cl,
+              monetary.variables = c("Z"),
+              nreplications = 1000,
+              confidence.level = 0.95,
+              method = "kr",
+              seed=123)
+mwtp.cl
+
+# Okay this is the point where I realized I am getting the code and it is not 
+# helping me get the statistics. I think I need to spend some time this weekend 
+# reading earlier chapters in the textbook. I will call it here with the code for
+# now, even though I could go farther, I dont think it will help me until I 
+# have a better understanding of these measures and methods. 
